ee7189db10ab18b85391661207012215134fe2c3
Alexandre Girard <git@alexgirard.com>
2011-12-29 13:32:33 +0100

    refactor generating code using Message class - begin to hide months

--- relinkwish.rb
+++ relinkwish.rb
@@ -3,13 +3,79 @@ require 'rexml/document'
 require 'date'
 require 'htmlentities'
 
+class Message
+
+  attr_reader :sender, :link, :time, :content, :content_type, :content_id
+
+  def initialize(line)
+    coder = HTMLEntities.new
+    if line.match(/sender="(.[^"]*)" time="(.[^"]*)" .*<div><a href="(http:.[^"]*)">.*<\/a> (.*)<\/div>/)
+      @sender = $1
+      @time = Date.parse($2)
+      @link = $3
+      text = coder.encode(coder.decode($4).gsub(/<(.*?)>/, ''), :decimal)
+      @content = text unless text.match(/.*tetalab(devel)*\/.*/)
+
+      href_ext = @link[-4, 4]
+      @content_type = "image" if href_ext==".jpg" || href_ext==".png" || href_ext==".gif" || href_ext==".svg"
+      @content_type = "audio" if href_ext==".mp3" || href_ext==".wav" || href_ext==".oga"
+      @content_type = "video" if href_ext==".mp4" || href_ext==".ogv" || href_ext==".ogg" || @link[-5, 5]==".webm" || href_ext==".mov"
+
+      if vimeo = @link.scan(/[http|https]\:\/\/vimeo\.com\/.*(\d{8})$/)[0]
+        @content_id = vimeo[0]
+        @content_type = "vimeo"
+      end
+
+      if youtube = @link.scan(/[http|https]\:\/\/www\.youtube\.com\/watch\?v=(.{11})/)[0]
+        @content_id = youtube[0]
+        @content_type = "youtube"
+      end
+    end
+  end
+
+  def format_header(show_content = true)
+    output = "<li class='month_before year_#{@time.year} "
+    output += "hidden" unless show_content
+    output += "'></li><a name='#{@time.strftime("%Y-%m")}'></a>"
+    output += "<li class='month "
+    output += "hidden" unless show_content
+    output += "' id='#{@time.strftime("%Y-%m")}'><span class='m'>#{@time.strftime("%B")}</span>"
+    output += "<span class='y'>#{@time.year}</span></li>"
+    output += "<li class='month_after"
+    output += "hidden" unless show_content
+    output += "'></li>"
+    return output
+  end
+
+  def format_content(show_content = true)
+    output = "<li class='timespot "
+    output += "hidden" unless show_content
+    output +="'><div class='text'><p>"
+    output += "<a href=\"#{@link}\">#{@link}</a> #{@content}"
+
+    if @content_type
+      output += case @content_type
+      when "image" then '<br><img src="' + @link.gsub(/\"/, '&quot;') + '"><br>'
+      when "vimeo" then '<br><div style="height:238px"><iframe width=400 height=238 src="https://player.vimeo.com/video/' + @content_id + '?title=0&byline=0&portrait=0"></iframe></div>'
+      #when "youtube" then #output = '<br><div style="height:385px"><iframe width=640 height=385 src="https://www.youtube.com/embed/' + @content_id + '"></iframe></div>'
+      else ""
+      end
+    end
+
+    output += "</p></div><div class='long_date'>Posted on #{@time.strftime("%B %d, %Y at %H:%M")}</div>"
+    output += "<div class='date'>#{@time.strftime("%B %d")}</div></li>"
+
+    return output
+  end
+end
+
 class Relinkwish
-  @link_arr = []
+  @messages = []
   @file_arr = []
 
   def initialize
     puts "This tool is for removing urls from xml based chat logs"
-    @link_arr = []
+    @messages = []
   end
 
   def extract_links(line)
@@ -25,100 +91,63 @@ class Relinkwish
   def pull_all_urls(read_these)
     senders = ["alx.girard@gmail.com"]
     all_file_links = []
-    coder = HTMLEntities.new
     read_these.each do |f|
       File.open(f, "r") do |infile|
         while (line = infile.gets)
-          if line.match(/sender="(.[^"]*)" time="(.[^"]*)" .*<div><a href="(http:.[^"]*)">.*<\/a> (.*)<\/div>/)
-            sender = $1
-            time = Date.parse($2)
-            link = $3
-            text = coder.encode(coder.decode($4).gsub(/<(.*?)>/, ''), :decimal)
-            if senders.include? sender
-              unless text.match(/.*tetalab(devel)*\/.*/)
-                data = {:time => time, :link => link, :text => text}
-                @link_arr << data
-              end
-            end
+          message = Message.new line
+          if senders.include? message.sender
+            @messages << message
           end
         end
       end
     end
-    @link_arr.sort!{|a, b| b[:time] <=> a[:time]}
-    #@link_arr = all_file_links.flatten.uniq
-    puts "Complete! #{@link_arr.size} links"
+    @messages.sort!{|a, b| b.time <=> a.time}
+    puts "Complete! #{@messages.size} links"
   end
 
-  def build_link_file
-    strip
-    
-    index = File.open("index.html", "w+")
-
-    File.open("header.html", "r") do |header|
-      while(line = header.gets)
-        index.puts(line)
+  def append_file(dest_file, origin_file)
+    File.open(origin_file, "r") do |content|
+      while(line = content.gets)
+        dest_file.puts(line)
       end
     end
+  end
 
+  def build_link_list(index)
     index.puts("<ul class='vertical ' id='timeline'>")
     timeline_years = []
     current_month = 12
     first = true
-    @link_arr.each do |link|
-      if link[:time].month != current_month || first
-        month_title = "<li class='month_before year_#{link[:time].year}'></li><a name='#{link[:time].strftime("%Y-%m")}'></a>"
-        month_title += "<li class='month' id='#{link[:time].strftime("%Y-%m")}'><span class='m'>#{link[:time].strftime("%B")}</span>"
-        month_title += "<span class='y'>#{link[:time].year}</span></li>"
-        month_title += "<li class='month_after'></li>"
-        index.puts(month_title+"\n")
-        current_month = link[:time].month
+    show_content = true
+    @messages.each do |message|
+      if message.time.month != current_month || first
+        show_content = false unless first
+        index.puts(message.format_header(show_content)+"\n")
+        current_month = message.time.month
         first = false
       end
-
-      final_link = "<li class='timespot'><div class='text'><p>"
-      final_link += "<a href=\"#{link[:link]}\">#{link[:link]}</a> #{link[:text]}"
-
-      href_ext = link[:link][-4, 4];
-      #
-      # image
-      if href_ext==".jpg" || href_ext==".png" || href_ext==".gif" || href_ext==".svg"
-        final_link += '<br><img src="' + link[:link].gsub(/\"/, '&quot;') + '"><br>'
-      end
-
-      # YouTube
-      #if youtube = link[:link].scan(/[http|https]\:\/\/www\.youtube\.com\/watch\?v=(.{11})/)[0]
-      #  youtube_id = youtube[0]
-      #  final_link += '<br><div style="height:385px"><iframe width=640 height=385 src="https://www.youtube.com/embed/' + youtube_id + '"></iframe></div>'
-      #end
-
-      # Vimeo
-      if vimeo = link[:link].scan(/[http|https]\:\/\/vimeo\.com\/.*(\d{8})$/)[0]
-        vimeo_id = vimeo[0]
-        final_link += '<br><div style="height:238px"><iframe width=400 height=238 src="https://player.vimeo.com/video/' + vimeo_id + '?title=0&byline=0&portrait=0"></iframe></div>'
-      end
-
-      final_link += "</p></div><div class='long_date'>Posted on #{link[:time].strftime("%B %d, %Y at %H:%M")}</div>"
-      final_link += "<div class='date'>#{link[:time].strftime("%B %d")}</div></li>"
-      index.puts(final_link+"\n")
+      index.puts(message.format_content(show_content)+"\n")
     end
     index.puts("</ul>")
+  end
 
+  def build_calendar_menu_list(index)
     current_month = 12
     count = 0
     first = true
     timeline_footer = "<footer id='timeline_footer'><nav id='navigator'>"
-    @link_arr.reverse.each do |link|
-      if link[:time].month != current_month
+    @messages.reverse.each do |message|
+      if message.time.month != current_month
         if current_month == 12
           unless first
             timeline_footer += "</ul></section>"
           end
-          timeline_footer += "<section class='year'><h2 class='year'><a href=\"#year-#{link[:time].year}\" class=\"#{link[:time].year}\">#{link[:time].year}</a></h2><ul>"
+          timeline_footer += "<section class='year'><h2 class='year'><a href=\"#year-#{message.time.year}\" class=\"#{message.time.year}\">#{message.time.year}</a></h2><ul>"
           first = false
         end
 
-        timeline_footer += "<li><a href=\"##{link[:time].strftime("%Y-%m")}\">#{link[:time].strftime("%B")}</a></li>"
-        current_month = link[:time].month
+        timeline_footer += "<li><a href=\"##{message.time.strftime("%Y-%m")}\">#{message.time.strftime("%B")}</a></li>"
+        current_month = message.time.month
         count = 0
       else
         count += 1
@@ -126,36 +155,19 @@ class Relinkwish
     end
     timeline_footer += "</ul></section></nav></footer>"
     index.puts(timeline_footer)
+  end
 
-    File.open("footer.html", "r") do |footer|
-      while(line = footer.gets)
-        index.puts(line)
-      end
-    end
+  def build_link_file
+    strip
+    index = File.open("index.html", "w+")
 
+    append_file(index, "header.html")
+    build_link_list(index)
+    build_calendar_menu_list(index)
+    append_file(index, "footer.html")
 
     index.close
     puts "File is built"
-  end
-
-  def how_many?
-    return @link_arr.count
-  end
-
-  def view_a_url(num)
-    viewing = @link_arr[num]
-    viewing.gsub("href=\"", "").chomp("\"")
-  end
-
-  def output_messages_by_file_by_id(file_id)
-    traversable = REXML::Document.new(file)
-    chat = traversable.root
-    puts "FROM: #{chat.attributes["service"]}"
-    puts "----------------------------------"
-    puts chat.elements["message"].attributes["sender"]
-    traversable.elements.each("message") do |msg|
-      puts "#{msg.atrributes["sender"]} said __ :: __ #{msg}"
-    end
   end
 end
 
